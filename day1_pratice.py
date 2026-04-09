5977dcf5e0d04fe153afc87c47df7f3b48afb2f5
manisharajpurohit055-arch <manisharajpurohit055@gmail.com>
2026-04-10 00:57:27 +0530

    basics arithmethic and variables

---
+++ day1_pratice.py
@@ -0,0 +1,25 @@
+print("Hello")
+print(5+9)
+#adding a comment
+print("5+9")#in quotations it will take it as string not thge arithematic function
+print(5*4)
+print(3**4)#exponential function
+print(9/3)
+print(7-1)
+print(((4*3)+9)-18/6)#for multiple arithmatic function add brackets
+#variables
+test_variables=9*4
+print(test_variables)#no spaces in variables,cant strat by letters,it can be numbers,letters,_
+#manipulating variables
+test_variables=9*4
+print(test_variables)
+test_variables1=4+test_variables
+print(test_variables1)
+#using multiple variables
+num_years = 4
+days_per_year = 365 
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
