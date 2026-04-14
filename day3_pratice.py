9d7c68ae5c2d96f9877ea0b639385e28d8700faa
manisharajpurohit055-arch <manisharajpurohit055@gmail.com>
2026-04-14 15:44:40 +0530

    data types

---
+++ day3_pratice.py
@@ -0,0 +1,47 @@
+#3integer
+x=14
+print(type(x))
+#float 
+y=4.3212105748985414232010234898541222410055
+print(type(y))
+
+y1=22/7
+print(type(y1))
+
+y2=round(y1,15)#15 is for decimal  how much required
+print(y2)
+
+#boolean data types
+z1=1<4
+print(z1)
+print(type(z1))
+
+z2=1>88
+print(z2)
+print(type(z2))
+
+z1= not z2
+print(z1)
+
+#strings
+w = "Hello, Python!"
+print(w)
+print(type(w))
+print(len(w))
+
+w1=""
+print(w1)
+
+my_number = "1.12321"
+print(my_number)
+print(type(my_number))
+
+#concatenation of 2 strings 
+a1="abc"+"def"
+print(a1)
+print(type(a1))
+
+#mutiply strings only by int not by float 
+a2="achjckdmnb"*2
+print(a2)
+print(type(a2))
